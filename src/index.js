88cf8cb8aa451d8a92120f33d5f32623fbd38c6b
leksyib <leksyib14@gmail.com>
2019-02-18 12:08:17 +0100

    chore(#164029706): setup webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/default */
+
+import React from 'react';
+import { render } from 'react-dom';
+// import { Router, browserHistory } from 'react-router';
+
+
+render(
+    <h1> HEllow from react </h1>, document.getElementById('app')
+);
