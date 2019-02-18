c6b67818549d1f7171f392d8cedb8a91fa72ff06
leksyib <leksyib14@gmail.com>
2019-02-18 13:21:55 +0100

    chore(#164029821): convert authentication pages to react

--- src/components/header.js
+++ src/components/header.js
@@ -7,28 +7,28 @@ return(
     <span className='navbar-toggle' id='js-navbar-toggle'>
         <i className='fas fa-bars'></i>
     </span>
-    <Link to='index.html' className='logo'><h1 className='title'> iReporter </h1></Link>
+    <Link to='/' className='logo'><h1 className='title'> iReporter </h1></Link>
     <ul className='main-nav' id='js-menu'>
       <li>
-          <Link to='index.html' className='nav-links'>Home</Link>
+          <Link to='/' className='nav-links'>Home</Link>
       </li>
       <li>
-          <Link to='sign-in.html' className='nav-links'>Sign in</Link>
+          <Link to='/signin' className='nav-links'>Sign in</Link>
       </li>
       <li>
           <Link to='/signup' className='nav-links'>Sign up</Link>
       </li>
       <li>
-          <Link to='admin.html' className='nav-links'>Admin</Link>
+          <Link to='/admin' className='nav-links'>Admin</Link>
       </li>
       <li>
-          <Link to='new-record.html' className='nav-links'>New record</Link>
+          <Link to='/new-record' className='nav-links'>New record</Link>
       </li>
       <li>
-          <Link to='profile.html' className='nav-links'>My Profile</Link>
+          <Link to='/profile' className='nav-links'>My Profile</Link>
       </li>
       <li>
-          <Link to='sign-up.html' className='nav-links'>Logout</Link>
+          <Link to='/' className='nav-links'>Logout</Link>
       </li>
     </ul>
     </nav>
