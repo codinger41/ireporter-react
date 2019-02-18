5913e60b7a606948c849946454218bdade0cea16
leksyib <leksyib14@gmail.com>
2019-02-18 13:12:25 +0100

    chore(#164029821): convert authentication pages to react

--- src/components/header.js
+++ src/components/header.js
@@ -2,35 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return(
+return(
     <nav className='navbar'>
-      <span className='navbar-toggle' id='js-navbar-toggle'>
+    <span className='navbar-toggle' id='js-navbar-toggle'>
         <i className='fas fa-bars'></i>
-      </span>
-      <Link to='index.html' className='logo'><h1 className='title'> iReporter </h1></Link>
-      <ul className='main-nav' id='js-menu'>
-          <li>
-              <Link to='index.html' className='nav-links'>Home</Link>
-          </li>
-          <li>
-            <Link to='sign-in.html' className='nav-links'>Sign in</Link>
-          </li>
-          <li>
-              <Link to='sign-up.html' className='nav-links'>Sign up</Link>
-          </li>
-          <li>
-              <Link to='admin.html' className='nav-links'>Admin</Link>
-          </li>
-          <li>
-              <Link to='new-record.html' className='nav-links'>New record</Link>
-          </li>
-          <li>
-              <Link to='profile.html' className='nav-links'>My Profile</Link>
-          </li>
-          <li>
-              <Link to='sign-up.html' className='nav-links'>Logout</Link>
-          </li>
-      </ul>
+    </span>
+    <Link to='index.html' className='logo'><h1 className='title'> iReporter </h1></Link>
+    <ul className='main-nav' id='js-menu'>
+      <li>
+          <Link to='index.html' className='nav-links'>Home</Link>
+      </li>
+      <li>
+          <Link to='sign-in.html' className='nav-links'>Sign in</Link>
+      </li>
+      <li>
+          <Link to='/signup' className='nav-links'>Sign up</Link>
+      </li>
+      <li>
+          <Link to='admin.html' className='nav-links'>Admin</Link>
+      </li>
+      <li>
+          <Link to='new-record.html' className='nav-links'>New record</Link>
+      </li>
+      <li>
+          <Link to='profile.html' className='nav-links'>My Profile</Link>
+      </li>
+      <li>
+          <Link to='sign-up.html' className='nav-links'>Logout</Link>
+      </li>
+    </ul>
     </nav>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './containers/home';
+import Signup from './containers/signup';
+import Signin from './containers/signin';
 
 const Router = () => (
   <div>
     <Switch>
-      <Route path='/' component={Home} />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/signup' component={Signup} />
+      <Route exact path='/signin' component={Signin} />
     </Switch>
   </div>
 )
